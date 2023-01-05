c329afb4671f817cb787cecd9f5973f783e3763f
Luan Lima <luan_lima2011@hotmail.com>
2023-01-05 17:10:30 -0300

    Adicionado classe java e entrada de dados

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import Entities.Bill;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+  public static void main(String[] args) throws Exception {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    Bill bill = new Bill();
+
+    System.out.print("Sexo(F/M): ");
+    bill.gender = sc.next().charAt(0);
+
+    System.out.print("Quantidade de cevejas: ");
+    bill.beer = sc.nextInt();
+
+    System.out.print("Quantidade de refrigerantes: ");
+    bill.softDrink = sc.nextInt();
+
+    System.out.print("Quantidade de espetinhos: ");
+    bill.barbecue = sc.nextInt();
+  }
 }
---
+++ src/Entities/Bill.java
@@ -0,0 +1,12 @@
+package Entities;
+
+public class Bill {
+    public char gender;
+    public int beer;
+    public int barbecue;
+    public int softDrink;
+
+    public double cover() {
+        
+    }
+}
